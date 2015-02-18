0c348d1e546bdf4861683cb94e7d5492c2c30574
araborn <araborn.email@web.de>
2015-02-18 15:24:14 +0100

    pageEdit#10

--- resources/js/code.js
+++ resources/js/code.js
@@ -9,13 +9,31 @@ function hide(id) {
 /*"autores","documentos","publicacoes","genero","cronologia","bibliografia","projeto" */
 
 function u_nav(navID) {
-        var IDs = ["nav_autores",
-        "nav_documentos","nav_documentos_sub","nav_documentos_sub_1","nav_documentos_sub_2","nav_documentos_sub_3","nav_documentos_sub_4","nav_documentos_sub_5","nav_documentos_sub_6","nav_documentos_sub_7","nav_documentos_sub_8","nav_documentos_sub_9",
-        "nav_publicacoes","nav_genero",
-        "nav_cronologia","nav_cronologia_sub","nav_cronologia_sub_1","nav_cronologia_sub_2","nav_cronologia_sub_3","nav_cronologia_sub_4",
-        "nav_bibliografia","nav_projeto"]
+        var IDs = [
+        "nav_autores",
+        "nav_bibliografia",
+        "nav_documentos",
+        "nav_documentos_sub",
+        "nav_documentos_sub_1",
+        "nav_documentos_sub_2",
+        "nav_documentos_sub_3",
+        "nav_documentos_sub_4",
+        "nav_documentos_sub_5",
+        "nav_documentos_sub_6",
+        "nav_documentos_sub_7",
+        "nav_documentos_sub_8",
+        "nav_documentos_sub_9",
+        "nav_publicacoes",
+        "nav_genero",
+        "nav_cronologia",
+        "nav_cronologia_sub",
+        "nav_cronologia_sub_0",
+        "nav_cronologia_sub_1",
+        "nav_cronologia_sub_2",
+        "nav_cronologia_sub_3",        
+        "nav_projeto"]
        
-       for (i = 0; i<IDs.length; i++) {
+       for (i = 0; i<=IDs.length; i++) {
         if(document.getElementById(navID).style.display = "none" && IDs[i].contains(navID)) {
             document.getElementById(navID).style.display = "inline-block"; 
              if(navID.contains("nav_documentos")) {
@@ -26,6 +44,7 @@ function u_nav(navID) {
                     document.getElementById("nav_cronologia").style.display = "inline-block";
                         document.getElementById("nav_cronologia_sub").style.display = "inline-block";
                 }
+                
         }
         else {
             document.getElementById(IDs[i]).style.display = "none";
